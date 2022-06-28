742a2cd3466625a5427747b8f586ffa0a695c3a3
Jack12qe <jack12qe@gmail.com>
2022-06-28 23:33:44 +0900

    creating admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-# Register your models here.
 
-
-# make model to form
+# Arrange model to fieldset on admin panel
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
