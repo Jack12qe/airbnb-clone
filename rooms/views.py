5afa0cf57b9a590e9b0ff3bb10cf7cb9882d6a61
Jack12qe <jack12qe@gmail.com>
2022-07-02 23:58:25 +0900

    base view is overed

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from . import models
 
-# Create your views here.
+
+class HOME_VIEW(ListView):
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.all()
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
