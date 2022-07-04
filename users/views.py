1c4e8d8ed3c669531c80fa03a3f5cccf94ade896
JH LEE <jack12qe@gmail.com>
2022-07-04 17:26:29 +0900

    validating email

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+    """LoginView Definition"""
+
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "francismurillo@example.com"})
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
