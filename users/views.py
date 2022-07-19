0acd210795196b7b7ae0d902671ec1d91ed87811
Jack12qe <jack12qe@gmail.com>
2022-07-19 09:13:45 +0900

    translation

--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
 import os
+import requests
+from config import settings
+from django.utils import translation
+from django.http import HttpResponse
 from django.views.generic import FormView, DetailView, UpdateView
 from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import redirect
@@ -8,7 +12,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.files.base import ContentFile
-import requests
 from . import forms, models, mixins
 
 
@@ -286,3 +289,12 @@ def switch_hosting(request):
     except KeyError:
         request.session["is_hosting"] = True
     return redirect(reverse("core:home"))
+
+
+def switch_language(request):
+    lang = request.GET.get("lang", None)
+    if lang is not None:
+        translation.activate(lang)
+        response = HttpResponse(status=200)
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang)
+    return response
